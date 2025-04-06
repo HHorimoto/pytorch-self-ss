0557c198b080c34f359d326806752c3288587a13
HHorimoto <81685394+HHorimoto@users.noreply.github.com>
2025-04-06 12:49:29 +0900

    add: simclr

--- src/models/models.py
+++ src/models/models.py
@@ -7,9 +7,9 @@ class CNN(nn.Module):
         super().__init__()
         self.conv1 = nn.Conv2d(3, 16*widen_factor, kernel_size=3, stride=1, padding=1)
         self.conv2 = nn.Conv2d(16*widen_factor, 32*widen_factor, kernel_size=3, stride=1, padding=1)
-        self.fc1 = nn.Linear(8*8*32*widen_factor, 1024*widen_factor)
-        self.fc2 = nn.Linear(1024*widen_factor, 1024*widen_factor)
-        self.fc3 = nn.Linear(1024*widen_factor, 10)
+        self.l1 = nn.Linear(8*8*32*widen_factor, 1024*widen_factor)
+        self.l2 = nn.Linear(1024*widen_factor, 1024*widen_factor)
+        self.l3 = nn.Linear(1024*widen_factor, 10)
         self.act = nn.ReLU()
         self.pool = nn.MaxPool2d(2, 2)
     
@@ -17,9 +17,9 @@ class CNN(nn.Module):
         x = self.pool(self.act(self.conv1(x)))
         x = self.pool(self.act(self.conv2(x)))
         x = x.view(x.size()[0], -1)
-        x = self.act(self.fc1(x))
-        x = self.act(self.fc2(x))
-        x = self.fc3(x)
+        x = self.act(self.l1(x))
+        x = self.act(self.l2(x))
+        x = self.l3(x)
         return x
 
 class Identity(nn.Module):
@@ -28,3 +28,23 @@ class Identity(nn.Module):
     
     def forward(self, x):
         return x
+    
+class SimCLR(nn.Module):
+    def __init__(self, encoder, projection_dim=128):
+        super(SimCLR, self).__init__()
+        self.encoder = encoder
+        self.n_features = encoder.l3.in_features
+        self.encoder.l3 = Identity()
+
+        self.projection_dim = projection_dim
+        self.projector = nn.Sequential(nn.Linear(self.n_features, self.n_features, bias=False),
+                                       nn.ReLU(),
+                                       nn.Linear(self.n_features, self.projection_dim, bias=False))
+        
+    def forward(self, x_i, x_j):
+        h_i = self.encoder(x_i)
+        h_j = self.encoder(x_j)
+
+        z_i = self.projector(h_i)
+        z_j = self.projector(h_j)
+        return z_i, z_j

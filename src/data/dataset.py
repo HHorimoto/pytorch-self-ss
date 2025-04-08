d1375c186e1a604204ac80dd6fe17fa19319a9e9
HHorimoto <81685394+HHorimoto@users.noreply.github.com>
2025-04-08 18:47:09 +0900

    add: sup func

--- src/data/dataset.py
+++ src/data/dataset.py
@@ -34,3 +34,31 @@ def create_unsupdataset(root='./data/', download=True, batch_size=256):
     unsup_loader = torch.utils.data.DataLoader(unsup_trainset, batch_size=batch_size, shuffle=True,
                                                num_workers=2, pin_memory=True, worker_init_fn=worker_init_fn, drop_last=True)    
     return unsup_loader
+
+def x_u_split(dataset, test_size=1000, seed=42):
+    sss = StratifiedShuffleSplit(n_splits=1, test_size=test_size, random_state=seed) # same percentage of samples
+    sss = sss.split(list(range(len(dataset))), dataset.targets)
+    _, label_idxs = next(sss)
+
+    return label_idxs
+
+def create_supdataset(root='./data/', download=True, batch_size=16):
+    train_transform = Compose([ToTensor(),
+                               Normalize((0.4914, 0.4822, 0.4465), (0.2470, 0.2435, 0.2616)),])
+    test_transform = Compose([ToTensor(),
+                              Normalize((0.4914, 0.4822, 0.4465), (0.2470, 0.2435, 0.2616)),])
+
+    trainset = datasets.CIFAR10(root=root, train=True,  download=download, transform=train_transform)
+    testset  = datasets.CIFAR10(root=root, train=False, download=download, transform=test_transform)
+
+    label_idxs = x_u_split(trainset)
+    train_subset = Subset(trainset, label_idxs)
+    labels = [trainset.targets[idx] for idx in label_idxs]
+    train_subset.train_labels = labels
+
+    train_loader = torch.utils.data.DataLoader(train_subset, batch_size=batch_size, shuffle=True,
+                                               num_workers=2, pin_memory=True, worker_init_fn=worker_init_fn, drop_last=True)
+    test_loader = torch.utils.data.DataLoader(testset, batch_size=batch_size, shuffle=False,
+                                               num_workers=2, pin_memory=True, worker_init_fn=worker_init_fn, drop_last=False)
+    
+    return train_loader, test_loader
